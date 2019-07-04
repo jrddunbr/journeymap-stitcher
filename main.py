f6812a6b5eccd9f79a94719b7a55af6db609fae1
Jared Dunbar <jrddunbr@gmail.com>
2019-07-04 14:15:06 -0400

    Fixed some problems with maps with the origin contained, will now not miss a row. May have broken it otherwise. Math is hard.

--- main.py
+++ main.py
@@ -4,65 +4,62 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-#
-# -- Useful Library Functions
-#
-
-# open image
-# img = Image.open(file)
-
-# get image size
-
-
-# create new image
-# img = Image.new('RGB', (width, height))
-
-# paste images in place
-# img.paste(im=img, box=(x, y))
-
-# show image
-
-# img.show()
-
-# save image
-# img.save(file)
+def getImageSize(filename):
+    img = Image.open(filename)
+    return img.size[0]
 
 INPUTPATH = "./input/"
-
 filelist = [f for f in listdir(INPUTPATH) if isfile(join(INPUTPATH, f))]
+clist = [(int(x.split(",")[0]), int(x.split(",")[1])) for x in [f.split(".",1)[0] for f in filelist]]
+TILE_SIZE = getImageSize(INPUTPATH + filelist[0])
 
-img = Image.open(INPUTPATH + filelist[0])
-TILE_WIDTH, TILE_HEIGHT = img.size
+# coordinate directions
+# x,z
+# -,-   +,-
+#
+# -,+   +,+
 
-bigX = 0
-bigY = 0
-smallX = 0
-smallY = 0
+bottom = -1000
+top = 1000
+left = 1000
+right = -1000
 
-for f in filelist:
-    coord = f.split(".")
-    ints = coord[0].split(",")
-    x = int(ints[0])
-    y = int(ints[1])
-    if x > bigX:
-        bigX = x
-    if x < smallX:
-        smallX = x
-    if y > bigY:
-        bigY = y
-    if y < smallY:
-        smallY = y
+print(clist)
 
-print("image map spans from ({},{}) to ({},{})".format(smallX, smallY, bigX, bigY))
+for entry in clist:
+    x = entry[0]
+    z = entry[1]
+    if (x > right):
+        right = x
+    if (x < left):
+        left = x
+    if (z < top):
+        top = z
+    if (z > bottom):
+        bottom = z
 
-bigimg = Image.new('RGB', (TILE_HEIGHT * (bigX + abs(smallX)), TILE_WIDTH * (bigY + abs(smallY))))
+# These may be wrong for things that don't pass through the origin.
+width = abs(left) + abs(right) + 1
+height = abs(top) + abs(bottom) + 1
+
+print("({},{}) to ({},{})".format(left, top, right, bottom))
+
+bigimg = Image.new('RGB', (TILE_SIZE * width, TILE_SIZE * height))
 
 print("starting stitch")
 
-for x in range(smallX, bigX):
-    for y in range(smallY, bigY):
-        if "{},{}.png".format(x,y) in filelist:
-            img = Image.open(INPUTPATH + "{},{}.png".format(x, y))
-            bigimg.paste(im=img, box=((x + abs(smallX)) * TILE_HEIGHT, (y + abs(smallY)) * TILE_WIDTH))
+# again, comment about the origin
+for z in range(top, bottom+1):
+    for x in range(left, right+1):
+        fn = "{},{}.png".format(x, z)
+        if fn in filelist:
+            print("#", end="")
+            img = Image.open(INPUTPATH + fn)
+            bigimg.paste(im=img, box=((x + abs(left)) * TILE_SIZE, (z + abs(top)) * TILE_SIZE))
+        else:
+            print(" ", end="")
+    print()
 
 bigimg.save("output.png")
+
+print("done")
